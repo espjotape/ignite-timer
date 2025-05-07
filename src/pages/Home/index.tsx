e82b4513f6d7eb8de7e7cc2fc4a9179da71a5758
João Pedro <joaope.espindola2005@gmail.com>
2025-05-07 10:40:40 -0300

    Validating forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,19 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import { HomeContainer, FormContainer, CountdownContiner, Separator, StartCountdownButton, TaskInput, MinutesAmountInput } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60),
+})
+
 export function Home() {
- const { register, handleSubmit, watch } = useForm();
+ const { register, handleSubmit, watch } = useForm({
+  resolver: zodResolver(newCycleFormValidationSchema)
+ });
 
  function handleCreateNewCycle(data: any) {
   console.log(data)
